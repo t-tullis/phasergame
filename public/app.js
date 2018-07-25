ae9b20093da95f69468e2c1a1930896fbd752d86
t-tullis <t.tullis34@gmail.com>
2018-07-25 04:40:26 -0700

    Added more game logic

--- public/app.js
+++ public/app.js
@@ -37,6 +37,8 @@ function preload ()
 
 var platforms;
 var player;
+var score = 0;
+var scoreText;
 
 function create ()
 {
@@ -57,6 +59,7 @@ function create ()
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
+  //Creates sprite animation
   this.anims.create({
     key: 'left',
     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3}),
@@ -76,7 +79,31 @@ function create ()
     frameRate: 10,
     repeat: -1
   });
+
+  //Create stars
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+
+  this.physics.add.collider(stars, platforms)
+  this.physics.add.overlap(player, stars, collectStar, null, this)
+
+  //Create scoreboard
+  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+  //Create Bombs
+  bombs = this.physics.add.group();
+
+  this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
+
 
 
 function update ()
@@ -100,9 +127,43 @@ else{
     player.anims.play('turn');
 }
 
-if (cursors.up.isDown && player.body.touching.down){
-    player.setVelocityY(-330);
+if (cursors.space.isDown && player.body.touching.down){
+    player.setVelocityY(-310);
 }
 
 }
+
+function collectStar (player, star)
+{
+  star.disableBody(true, true);
+
+  score += 10;
+  scoreText.setText('Score: ' + score);
+
+  if (stars.countActive(true) === 0)
+  {
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    var bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+}
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
+
 
